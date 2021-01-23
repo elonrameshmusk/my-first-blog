61627f2d16855205193091517885c00971a5d05d
Bagadi Venkat Ramesh <bagadi.venkat@iitg.ac.in>
2021-01-23 15:09:21 +0530

    Change the HTML for the site

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4)33r-65#1zibjlo0f*9vv7_ubrhsz)!^8(vy8-zd#083%ubt3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
